e2b78194f8ecf8fd9cca279c832f6ad8b1b9c8ad
= <akshay7.ch@gmail.com>
2019-06-02 00:48:57 +0530

    Files Added

---
+++ src/main/java/com/geo/search/GeoSearchApplication.java
@@ -0,0 +1,13 @@
+package com.geo.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoSearchApplication.class, args);
+	}
+
+}
